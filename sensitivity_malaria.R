8847c7351069a82547924e9a4ee40406cac8e3ae
Juliette Unwin <ettieunwin@users.noreply.github.com>
2022-11-24 11:32:00 +0000

    Adds example of the malaria sensitivity

--- sensitivity_malaria.R
+++ sensitivity_malaria.R
@@ -3,33 +3,36 @@ library(odin)
 library(tidyverse)
 
 # The file name of the model
-model <- "r0/exponential.R"
+model <- "malaria/basic.R"
 # Compiles the model
 mod_code <- odin::odin(model)
 # Define times to look at
-t = 1:20
+t = 1:5000
 
 # Range of inputs
-N_inits = 10:20
+ms = seq(from = 1, to = 10, length.out = 1001)
 
 # Function to run the model for each parameter set
-f <- function(N_init) {
-  mod <- mod_code$new(user = list(N_init = N_init))
+f <- function(m) {
+  mod <- mod_code$new(user = list(m = m))
   # Change value you want sensitivity over here
-  output = mod$run(t)[, "N"]  
+  output = mod$run(t)[, "I_h"]  
   return (output)
 }
 
 # Runs model for each of your input parameters
-output = data.frame(sapply(N_inits, f))
-names(output) = N_inits
+output = data.frame(sapply(ms, f))
+names(output) = ms
 output$t = t
 
 # Changes output to long format
-output_long = gather(output, key = parameter_value, value = value, -t)
+output_long = output %>%
+  gather(key = parameter_value, value = value, -t) %>% 
+  filter(t == max(t))
+output_long$parameter_value = as.numeric(output_long$parameter_value)
 
 # Makes plot
 p <- ggplot(output_long) + 
-  geom_line(aes(t, value, colour = parameter_value))+
+  geom_line(aes(parameter_value, value))+
   theme_bw()
 print(p)
